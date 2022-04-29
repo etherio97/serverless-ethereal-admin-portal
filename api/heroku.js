b6b43f1079ff8f532df41489585fc98e2c8ff04b
etherio97 <ethereal97@gmail.com>
2022-04-29 16:44:41 +0630

    x

--- api/heroku.js
+++ api/heroku.js
@@ -1,15 +1,15 @@
 const app = require('../src/app');
 const service = require('../src/services/heroku.service');
 
-app.get('/apps', async (req, res) => {
+app.get('/heroku/apps', async (req, res) => {
   res.json(await service.getApps());
 });
 
-app.get('/apps/:id', async (req, res) => {
+app.get('/heroku/apps/:id', async (req, res) => {
   res.json(await service.getApp(req.params.id));
 });
 
-app.get('/app-state', async (req, res) => {
+app.get('/heroku/app-state', async (req, res) => {
   res.json(await service.getAppState(req.query.apps || ''));
 });
 
