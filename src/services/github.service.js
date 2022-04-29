98bd7684c4c12e338c87cf24651e7d64a8efd3bb
etherio97 <ethereal97@gmail.com>
2022-04-29 21:10:57 +0630

    x

--- src/services/github.service.js
+++ src/services/github.service.js
@@ -1,20 +1,15 @@
 const { GITHUB_API, GITHUB_SECRET } = require('../config');
 const { default: axios } = require('axios');
 
-const headers = {
-  Accept: 'application/vnd.github.v3+json',
-  Authenication: 'token ' + GITHUB_SECRET,
-};
-
 class GithubService {
   getUser(user) {
     return this._sendApi('GET', ['users', user]);
   }
-  
+
   getUserRepos(user, params = {}) {
     return this._sendApi('GET', ['users', user, 'repos'], null, params);
   }
-  
+
   getRepo(user, repo) {
     return this._sendApi('GET', ['repos', user, repo]);
   }
@@ -25,6 +20,10 @@ class GithubService {
     for (let key in params) {
       url.searchParams.append(key, params[key]);
     }
+    const headers = {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: 'token ' + GITHUB_SECRET,
+    };
     const options = {
       method,
       url: url.toString(),
