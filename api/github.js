16d13ba3d186cf366bf9b9a6ba2d1c5f96f0a57e
etherio97 <ethereal97@gmail.com>
2022-04-29 20:48:52 +0630

    x

---
+++ api/github.js
@@ -0,0 +1,37 @@
+const app = require('../src/app');
+const service = require('../src/services/github.service');
+
+app.get('/github/:user/repos', async (req, res) => {
+  try {
+    const { user } = req.body;
+    res.json(await service.getUserRepos(user, req.query));
+  } catch (e) {
+    const response = e.response || {};
+    res.status(response.status || 500);
+    res.json(response.data || { error: e.message });
+  }
+});
+
+app.get('/github/:user', async (req, res) => {
+  try {
+    const { user } = req.body;
+    res.json(await service.getUser(user));
+  } catch (e) {
+    const response = e.response || {};
+    res.status(response.status || 500);
+    res.json(response.data || { error: e.message });
+  }
+});
+
+app.get('/github/:user/:repo', async (req, res) => {
+  try {
+    const { user, repo } = req.body;
+    res.json(await service.getRepo(user, repo));
+  } catch (e) {
+    const response = e.response || {};
+    res.status(response.status || 500);
+    res.json(response.data || { error: e.message });
+  }
+});
+
+module.exports = app;
---
+++ src/services/github.service.js
@@ -0,0 +1,38 @@
+const { GITHUB_API, GITHUB_SECRET } = require('../config');
+const { default: axios } = require('axios');
+
+const headers = {
+  Accept: 'application/vnd.github.v3+json',
+  Authenication: 'token ' + GITHUB_SECRET,
+};
+
+class GithubService {
+  getUser(user) {
+    return this._sendApi('GET', ['users', user]);
+  }
+  
+  getUserRepos(user, params = {}) {
+    return this._sendApi('GET', ['users', user, 'repos'], null, params);
+  }
+  
+  getRepo(user, repo) {
+    return this._sendApi('GET', ['repos', user, repo]);
+  }
+
+  _sendApi(method, path = [], data = null, params = {}) {
+    const url = new URL(GITHUB_API);
+    url.pathname = path.join('/');
+    for (let key in params) {
+      url.searchParams.append(key, params[key]);
+    }
+    const options = {
+      method,
+      url: url.toString(),
+      data,
+      headers,
+    };
+    return axios(options).then((res) => res.data);
+  }
+}
+
+module.exports = new GithubService();
