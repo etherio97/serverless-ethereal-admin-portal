fd1cd84e384659e90e8751614e0d11c2f05ae8ee
etherio97 <ethereal97@gmail.com>
2022-04-29 18:07:09 +0630

    cookie

--- api/firebase.js
+++ api/firebase.js
@@ -1,12 +1,32 @@
 const app = require('../src/app');
 const service = require('../src/services/firebase.service');
 
+const COOKIE_NAME = '__Secure-RefreshToken';
+
+app.get('/', async (req, res) => {
+  try {
+    const cookies = {
+      ...req.cookies,
+      ...req.signedCookies,
+    };
+    console.log(cookies);
+    const refreshToken = cookies[COOKIE_NAME];
+    res.send('ok');
+  } catch(e) {
+    const response = e.response || {};
+    res.status(response.status || 500);
+    res.json(response.data || { error: e.message });
+  }
+});
+
 app.post('/firebase/sign-in', async (req, res) => {
   try {
     const { email, password } = req.body;
-    res.json(await service.signInWithEmailAndPassword(email, password));
+    const credential = await service.signInWithEmailAndPassword(email, password);
+    res.setHeader('set-cookie', `${COOKIE_NAME}=${credential.refreshToken}; Secure; HttpOnly`);
+    res.json(credential);
   } catch(e) {
-    const response = e.response;
+    const response = e.response || {};
     res.status(response.status || 500);
     res.json(response.data || { error: e.message });
   }
@@ -17,7 +37,7 @@ app.post('/firebase/account', async (req, res) => {
     const { idToken } = req.body;
     res.json(await service.getUserData(idToken));
   } catch(e) {
-    const response = e.response;
+    const response = e.response || {};
     res.status(response.status || 500);
     res.json(response.data || { error: e.message });
   }
@@ -28,7 +48,7 @@ app.post('/firebase/token', async (req, res) => {
     const { refreshToken } = req.body;
     res.json(await service.getIdToken(refreshToken));
   } catch(e) {
-    const response = e.response;
+    const response = e.response || {};
     res.status(response.status || 500);
     res.json(response.data || { error: e.message });
   }
--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookie = require('cookie-parser');
 const app = express();
 
+app.use(cors());
+
+app.use(helmet());
+
 app.use(express.json());
+
+app.use(cookie());
 
 module.exports = app;
