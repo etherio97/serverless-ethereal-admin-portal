1bb68ff3d6ed0108a3ea29ee4f432adb81219cee
etherio97 <ethereal97@gmail.com>
2022-04-30 07:10:23 +0630

    x

---
+++ src/services/firebase.service.js
@@ -0,0 +1,57 @@
+const { FIREBASE_API_KEY, GOOGLE_SECURETOKEN, GOOGLE_IDENTITYTOOLKIT } = require('../config');
+const { default: axios } = require('axios');
+
+class FirebaseService {
+  getUserData(idToken) {
+    return this._sendApi(GOOGLE_IDENTITYTOOLKIT, 'v1/accounts:lookup', {
+      idToken,
+    });
+  }
+  
+  signUpWithEmailAndPassword(email, password) {
+    return this._sendApi(GOOGLE_IDENTITYTOOLKIT, 'v1/accounts:signUp', {
+      email,
+      password,
+      returnSecureToken: true,
+    });
+  }
+
+  signInWithEmailAndPassword(email, password) {
+    return this._sendApi(GOOGLE_IDENTITYTOOLKIT, 'v1/accounts:signInWithPassword', {
+      email,
+      password,
+      returnSecureToken: true,
+    });
+  }
+
+  updateUser({ idToken, email, password, displayName, photoURL, deleteAttribute }) {
+    return this._sendApi(GOOGLE_IDENTITYTOOLKIT, 'v1/accounts:signInWithPassword', {
+      idToken,
+      email,
+      password,
+      displayName,
+      photoURL,
+      deleteAttribute,
+      returnSecureToken: true,
+    });
+  }
+
+  getIdToken(refresh_token) {
+    return this._sendApi(GOOGLE_SECURETOKEN, 'v1/token', {
+      grant_type: 'refresh_token',
+      refresh_token,
+    });
+  }
+
+  _sendApi(baseUrl, path, data = null) {
+    const url = [baseUrl, path].join('/') + '?key=' + FIREBASE_API_KEY;
+    const options = {
+      method: 'POST',
+      url,
+      data,
+    };
+    return axios(options).then((res) => res.data);
+  }
+}
+
+module.exports = new FirebaseService();
