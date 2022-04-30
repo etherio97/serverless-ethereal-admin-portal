6ad8c16b6490606a5a1aa728f8114f93ae38b88e
etherio97 <ethereal97@gmail.com>
2022-04-30 21:33:24 +0630

    x

---
+++ api/analytics.js
@@ -0,0 +1,54 @@
+const { default: axios } = require('axios');
+const UAParser = require('ua-parser-js');
+const app = require('../src/app');
+
+const reportTo = (text, disable_notification = false) => 
+  axios
+    .post('https://api.telegram.org/bot' + process.env.TELEGRAM_BOT + '/sendMessage', {
+      chat_id: 2040191682,
+      text,
+      disable_notification,
+      parse_mode: 'markdown',
+    })
+    .then(({ data }) => data)
+    .catch(error => ({ error }));
+
+const getIpDetails = (ip) =>
+  axios
+  .get('https://ipinfo.io/' + ip + '/json')
+  .then(result => {
+    delete result.data.readme;
+    return result.data;
+  })
+  .catch(() => ({
+    ip, 
+    city: 'N/A', 
+    region: 'N/A', 
+    country: 'N/A', 
+    org: null,
+    timezone: null
+  }));
+
+app.get('/analytics', async (req, res) => {
+  let ua = req.headers['user-agent'];
+  let ip = (req.headers['x-forwarded-for'] || '').split(',')[0].trim();
+  let dev = new UAParser(ua);
+  let info = await getIpDetails(ip);
+  let text = [];
+  let { model, type } = dev.getDevice();
+  text.push(
+    'Someone try to access ```', req.originalUrl,
+    '``` ', 'from ```', ip, '``` (', info.org, ')',
+    ' from ', info.city, '/ ', info.country,
+  );
+  if (!['console', 'embeded'].includes(type)) {
+    text.push(
+      ' on ', dev.vendor, ' ', dev.getOS(),
+    );
+  }
+  await reportTo(text.join(''));
+  res.redirect('https://google.com');
+  res.end();
+});
+
+module.exports = app;
